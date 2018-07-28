214c72410954d4fbf881bbb8464a59105730407b
Ubuntu <ubuntu@ip-172-31-28-184.eu-west-1.compute.internal>
2018-07-28 08:35:27 +0000

    w2.t2.v1

--- js1/week2/2/index.js
+++ js1/week2/2/index.js
@@ -3,5 +3,18 @@
  * @returns {String}
  */
 module.exports = function (hashtags) {
+	
+	var	hashtagsUnicLowerCase = [];
+	
+	hashtags.forEach(doStringHashtags);
+	function doStringHashtags(item, index) {
+		item = item.toLowerCase();
+		if (hashtagsUnicLowerCase.indexOf(item) === -1 && item !== '') {
+				hashtagsUnicLowerCase.push(item) ;
+		}
+		
+	}
+	
+	return 	hashtagsUnicLowerCase.join(', ');
 
 };
