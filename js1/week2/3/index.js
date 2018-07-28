d6ab028d5fa6c628ef18c1ad9adf1b3c4ae462bd
Ubuntu <ubuntu@ip-172-31-28-184.eu-west-1.compute.internal>
2018-07-28 16:05:51 +0000

    w2.t3.v1

--- js1/week2/3/index.js
+++ js1/week2/3/index.js
@@ -6,5 +6,62 @@ var phoneBook = {};
  * @returns {*} - результат зависит от команды
  */
 module.exports = function (command) {
+	
+	var commandArray = [];
+		
+	var COMMAND_ADD = 'ADD',
+		COMMAND_REMOVE_PHONE = 'REMOVE_PHONE',
+		COMMAND_SHOW = 'SHOW';
+	
+	commandArray = command.split(' ');
+	if (COMMAND_ADD === commandArray[0]) {
+		return addContact(commandArray);
+	} else if (COMMAND_REMOVE_PHONE === commandArray[0]) {
+		return removePhone(commandArray);
+	} else if (COMMAND_SHOW === commandArray[0]) {
+		return showPhoneBook();
+	}
 
 };
+
+function addContact(commandArray) {
+	
+	if (phoneBook.hasOwnProperty(commandArray[1])) {
+		if (phoneBook[commandArray[1]].indexOf(commandArray[2] === -1)) { 
+			phoneBook[commandArray[1]] += ',' + commandArray[2];
+		}
+	} else {
+		phoneBook[commandArray[1]]=commandArray[2];
+	}
+
+	return phoneBook;
+}
+
+function removePhone(commandArray) {
+
+	var result = false;
+	
+	for (var key in phoneBook) {
+		if (phoneBook[key].indexOf(commandArray[1]) !== -1) {
+			result = true;
+			phoneBook[key] = phoneBook[key].replace(commandArray[1],'').replace(',,',',');
+		}
+	}
+
+	return result;
+}
+
+function showPhoneBook() {
+	
+	var result = [];
+
+	for (var key in phoneBook) {
+		
+		//add test for empty list of phone
+		if (phoneBook[key] !== '') {
+			result.push(key + ': ' + phoneBook[key].replace(/,/g, ', '));
+		}
+	}
+
+	return result.sort();
+}
