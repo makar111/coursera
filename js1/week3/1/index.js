98396edae4b05cf0db1b6f5df32200e9a1ce4c5a
Ubuntu <ubuntu@ip-172-31-28-184.eu-west-1.compute.internal>
2018-08-04 16:51:26 +0000

    w3t1v2

--- js1/week3/1/index.js
+++ js1/week3/1/index.js
@@ -3,5 +3,32 @@
  * @returns {Object}
  */
 module.exports = function (date) {
+	
+	function MyDate(strDate) {
+		this._date = new Date(strDate);
 
+		this.add = function() {
+			
+			return this;
+		};
+		
+		this.subtract = function() {
+			
+			return this;
+		};
+
+		Object.defineProperty(this, 'value', {
+			get: function() {
+				var regex = /.{8}$/;
+				return this._date.toISOString().replace(/T/,' ').replace(regex,'');
+			}
+		}
+		);		
+
+	};
+	
+	var myDate = new MyDate(date);
+	console.log(myDate.value);
+	return myDate;
+	
 };
