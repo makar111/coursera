ee2440145c81d1617f17aff41717cf642da2d345
Ubuntu <ubuntu@ip-172-31-28-184.eu-west-1.compute.internal>
2018-08-05 09:26:00 +0000

    w3t1v4

--- js1/week3/1/index.js
+++ js1/week3/1/index.js
@@ -5,52 +5,68 @@
 module.exports = function (date) {
 	
 	function MyDate(strDate) {
-		var date = new Date(strDate);
+		var date = new Date(strDate.replace(/ /,'T'));
 
-		this._year = date.getFullYear();
-		this._month = date.getMonth();
-		this._day = date.getDay();
+		this._years = date.getFullYear();
+		this._months = date.getMonth();
+		this._days = date.getDate();
 		this._hours = date.getHours();
-		this._minute = date.getMinutes();
-		this._second = date.getSeconds();
-		this._millisecond = date.getMilliseconds();
+		this._minutes = date.getMinutes();
+		this._seconds = date.getSeconds();
+		this._milliseconds = date.getMilliseconds();
+
+		
+		this.dateFormatMake = function(strDate) {
+			var regex = / /;
+			return strDate.replace(regex,'T');
+		};
+
 
 		this.makeDate = function() {
-			return new Date(this._year, this._month, this._day, this._hours, this._minute, this._second, this._millisecond);
+			return new Date(this._years, this._months, this._days, this._hours, this._minutes, this._seconds, this._milliseconds);
 		};
 
 
 		this.changeDate = function(timeInterval, timeIntervalType) {
-				this['_' + timeIntervalType] += timeInterval;
-				console.log(this.value);
+			if (!this.hasOwnProperty('_' + timeIntervalType)) {
+				throw new TypeError('Переданно неверное значеие');
+			}
+				
+			this['_' + timeIntervalType] += timeInterval;
 		};
 
 
 
 		this.add = function(timeInterval, timeIntervalType) {
+			if (timeInterval < 0) {
+				throw new TypeError('Переданно неверное значеие');
+			}
+
 			this.changeDate(timeInterval, timeIntervalType);
 			return this;
 		};
 		
-		this.subtract = function(subValue, subType) {
-//			this._date = new Date(this.date.getTime() + subValue);
+		this.subtract = function(timeInterval, timeIntervalType) {
+
+			if (timeInterval < 0) {
+				throw new TypeError('Переданно неверное значеие');
+			}
+
+
+			this.changeDate(timeInterval * (-1), timeIntervalType);
 			return this;
 		};
 
 		Object.defineProperty(this, 'value', {
 			get: function() {
 				var regex = /.{8}$/;
-	 		// makeDate();
 				return this.makeDate().toISOString().replace(/T/,' ').replace(regex,'');
 			}
-		}
-		);		
+		});		
 
 	};
 	
 	var myDate = new MyDate(date);
-	console.log(myDate.makeDate());
-	console.log(myDate.value);
 	return myDate;
 	
 };
