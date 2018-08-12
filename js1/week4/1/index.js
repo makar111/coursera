14d78d691261b578173c01780be3d277e9955ef3
Ubuntu <ubuntu@ip-172-31-28-184.eu-west-1.compute.internal>
2018-08-12 16:05:54 +0000

    w4t1v2

--- js1/week4/1/index.js
+++ js1/week4/1/index.js
@@ -5,13 +5,45 @@
  */
 function query(collection) {
 
+	var collectionNew = JSON.parse(JSON.stringify(collection));
+	
+	for(var i = 0; i < arguments.length; i++) {
+		if( arguments[i].name === 'filterIn') {
+			collectionNew = arguments[i](collectionNew);
+		}
+	}
+
+	for(var i = 0; i < arguments.length; i++) {
+		if( arguments[i].name === 'select') {
+			collectionNew = arguments[i](collectionNew);
+		}
+	}
+	
+	
+	return collectionNew;
 }
 
 /**
  * @params {String[]}
  */
 function select() {
+	
+	var selectFields = [].slice.call(arguments);
 
+	return function select(collection) {
+		
+		collection.forEach(function(item, i, arr) {
+			for (var key in item)  {
+				if(selectFields.indexOf(key)===-1 ) {
+					delete item[key];
+
+				}
+			}
+		});
+		
+		return collection;
+
+	};
 }
 
 /**
@@ -19,7 +51,14 @@ function select() {
  * @param {Array} values – Массив разрешённых значений
  */
 function filterIn(property, values) {
-
+	return function filterIn(collection){
+		var collectionNew = collection.filter(function(item) { 
+			return 	values.indexOf(item[property]) !== -1;
+		});
+		
+		return collectionNew;
+	
+	}
 }
 
 module.exports = {
