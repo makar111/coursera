aa376cabea57cf08d9e24ab93fb1915544a52da1
makar111 <makar111@mail.ru>
2018-08-18 19:47:56 +0500

    w5t1v1

--- js1/week5/checks.js
+++ js1/week5/checks.js
@@ -2,68 +2,60 @@
 var assert = require('assert');
 
 // Подключаем свою функцию
-var lib = require('./index.js');
+var emitter = require('./index.js');
 
-// Коллекция данных
-var friends = [
-    {
-        name: 'Сэм',
-        gender: 'Мужской',
-        email: 'luisazamora@example.com',
-        favoriteFruit: 'Картофель'
-    },
-    {
-        name: 'Эмили',
-        gender: 'Женский',
-        email: 'example@example.com',
-        favoriteFruit: 'Яблоко'
-    },
-    {
-        name: 'Мэт',
-        gender: 'Мужской',
-        email: 'danamcgee@example.com',
-        favoriteFruit: 'Яблоко'
-    },
-    {
-        name: 'Брэд',
-        gender: 'Мужской',
-        email: 'newtonwilliams@example.com',
-        favoriteFruit: 'Банан'
-    },
-    {
-        name: 'Шерри',
-        gender: 'Женский',
-        email: 'danamcgee@example.com',
-        favoriteFruit: 'Картофель'
-    },
-    {
-        name: 'Керри',
-        gender: 'Женский',
-        email: 'danamcgee@example.com',
-        favoriteFruit: 'Апельсин'
-    },
-    {
-        name: 'Стелла',
-        gender: 'Женский',
-        email: 'waltersguzman@example.com',
-        favoriteFruit: 'Картофель'
+// Определим объект для счетчика нотификаций
+var notifications = {
+    counter: 0,
+    count: function () {
+        this.counter++;
     }
-];
+};
 
-// Выполняем выборку и фильтрацию с помощью нашего конструктора
-var result = lib.query(
-    friends,
-    lib.select('name', 'gender', 'email'),
-    lib.filterIn('favoriteFruit', ['Яблоко', 'Картофель'])
-);
+// Определим для хранения логов
+var logger = {
+    logs: []
+};
 
-// Сравниваем полученный результат с ожидаемым
-assert.deepEqual(result, [
-    { name: 'Сэм', gender: 'Мужской', email: 'luisazamora@example.com' },
-    { name: 'Эмили', gender: 'Женский', email: 'example@example.com' },
-    { name: 'Мэт', gender: 'Мужской', email: 'danamcgee@example.com' },
-    { name: 'Шерри', gender: 'Женский', email: 'danamcgee@example.com' },
-    { name: 'Стелла', gender: 'Женский', email: 'waltersguzman@example.com' }
+// Подписываемся на событие new_notification и сразу оповещаем всех подписчиков
+emitter
+    .on('new_notification', notifications, notifications.count)
+    .on('new_notification', logger, function () {
+        this.logs.push('Произошло новое событие new_notification');
+    })
+    .on('new_notification', logger, function () {
+        // this указывает на logger
+        this.logs.push('Добавлена новая нотификация. Количество - ' + notifications.counter);
+    })
+    .emit('new_notification');
+
+// Проверяем количество нотификаций
+assert.equal(notifications.counter, 1, 'Получена одна нотификация');
+
+// В логе сохранено событие
+// Так как обработчик notifications.count отработал первым,
+//  в логах сохранено правильное количество нотификаций
+assert.deepEqual(logger.logs, [
+    'Произошло новое событие new_notification',
+    'Добавлена новая нотификация. Количество - 1'
+]);
+
+// На время отключаем логгирование, а затем снова включаем
+emitter
+    .off('new_notification', logger)
+    .emit('new_notification')
+    .on('new_notification', logger, function () {
+        this.logs.push('Новое событие new_notification!');
+    })
+    .emit('new_notification');
+
+// Проверяем количество нотификаций
+assert.equal(notifications.counter, 3, 'Получено три нотификации');
+// Проверяем, что логи были отключены, а затем снова подключены
+assert.deepEqual(logger.logs, [
+    'Произошло новое событие new_notification',
+    'Добавлена новая нотификация. Количество - 1',
+    'Новое событие new_notification!'
 ]);
 
 console.info('OK!');
--- js1/week5/index.js
+++ js1/week5/index.js
@@ -1,29 +1,26 @@
-/**
- * @param {Array} collection
- * @params {Function[]} – Функции для запроса
- * @returns {Array}
- */
-function query(collection) {
-
-}
-
-/**
- * @params {String[]}
- */
-function select() {
-
-}
-
-/**
- * @param {String} property – Свойство для фильтрации
- * @param {Array} values – Массив разрешённых значений
- */
-function filterIn(property, values) {
-
-}
-
 module.exports = {
-    query: query,
-    select: select,
-    filterIn: filterIn
+
+    /**
+     * @param {String} event
+     * @param {Object} subscriber
+     * @param {Function} handler
+     */
+    on: function (event, subscriber, handler) {
+
+    },
+
+    /**
+     * @param {String} event
+     * @param {Object} subscriber
+     */
+    off: function (event, subscriber) {
+
+    },
+
+    /**
+     * @param {String} event
+     */
+    emit: function (event) {
+
+    }
 };
